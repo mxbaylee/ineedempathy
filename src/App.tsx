ad1957fe5bbbdcc721d1b13e3a0c05a278fadd6c
Baylee Schmeisser <github.mx@baylee.dev>
2023-05-28 15:40:09 -0700

    📱 Add support for mobile touch.

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,16 @@ import React from 'react'
 import { Footer } from './components/Footer'
 import { CardTable } from './components/CardTable'
 import { HTML5Backend } from 'react-dnd-html5-backend'
+import { TouchBackend } from 'react-dnd-touch-backend'
 import { DndProvider } from 'react-dnd'
 import { CardDefinition } from './CardDefinition'
 import './App.css'
 
 function App() {
+  const isTouchDevice = (('ontouchstart' in window) || (navigator.maxTouchPoints > 0))
   return (
     <div className="App">
-      <DndProvider backend={HTML5Backend}>
+      <DndProvider backend={isTouchDevice ? TouchBackend : HTML5Backend}>
         <CardTable
           initialCards={CardDefinition}
         />
